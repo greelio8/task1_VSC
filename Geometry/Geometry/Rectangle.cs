1ca654747d3cc842c3d19b1e6fe880f7d7b987f8
greelio8 <dmitri.trubca@amdaris.com>
2021-02-16 15:34:22 +0200

    Add Circle class

--- Geometry/Geometry/Rectangle.cs
+++ Geometry/Geometry/Rectangle.cs
@@ -6,17 +6,17 @@ namespace Geometry
 {
      class Rectangle
      {
-          public int width { get; set; }
-          public int height { get; set; }
+          public int Width { get; set; }
+          public int Height { get; set; }
 
           public Rectangle(int width, int height) 
           {
-               this.width = width;
-               this.height = height;
+               this.Width = width;
+               this.Height = height;
           }
 
           public int Area() {
-               return width * height;
+               return Width * Height;
           }
      }
 }
